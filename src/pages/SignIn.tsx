8feee359e22bf9afb2d2d1d0f8dc6bb3cdb38357
Abhimanue <abhimanue80@gmail.com>
2024-10-23 00:22:32 +0530

    landing,signin,signup pages

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,9 +1,102 @@
-const SignIn = ()=>{
-    return (
-        <>
-        SignIn
-        </>
-    )
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+interface Inputs {
+  email: string;
+  password: string;
 }
 
-export default SignIn
+const SignIn = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    setIsLoading(true);
+    console.log(data);
+  };
+  return (
+    <>
+      <Card className="border-none shadow-none mx-auto max-w-sm">
+        <CardHeader className="text-center pb-10 ">
+          <CardTitle className="text-4xl">Welcome back</CardTitle>
+          <CardDescription>Enter your information to sign in</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <form className="grid gap-4" onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  className="border-slate-500 focus:border-none"
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  {...register("email", { required: "Email is required" })}
+                />
+                {errors.email && (
+                  <span className="text-red-500 text-xs">
+                    {errors.email.message}
+                  </span>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  className="border-slate-400 focus:border-none"
+                  id="password"
+                  type="password"
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                />
+                {errors.password && (
+                  <span className="text-red-500 text-xs">
+                    {errors.password.message}
+                  </span>
+                )}
+              </div>
+              <Button
+                type="submit"
+                className="bg-violet-800 text-white hover:bg-violet-900 hover:text-white"
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <div className="flex items-center justify-center">
+                    <div className="w-4 h-4 border-2 border-t-2 border-white border-t-transparent rounded-full animate-spin mr-2"></div>
+                    Loading...
+                  </div>
+                ) : (
+                  "Sign In"
+                )}
+              </Button>
+            </form>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Do not have an account?{" "}
+            <Link to="/sign-up" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default SignIn;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,9 +1,115 @@
-const SignUp = ()=>{
-    return (
-        <>
-        SignUp
-        </>
-    )
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+interface Inputs {
+  name: string;
+  email: string;
+  password: string;
 }
 
-export default SignUp
+const SignUp = () => {
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    setLoading(true);
+    console.log(data);
+  };
+  return (
+    <>
+      <Card className="border-none shadow-none mx-auto max-w-sm">
+        <CardHeader className="text-center pt-0 pb-10">
+          <CardTitle className="text-4xl">Get started</CardTitle>
+          <CardDescription>
+            Enter your information to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <form className="grid gap-4" onSubmit={handleSubmit(onSubmit)}>
+              <div className="grid gap-2">
+                <Label className="text-sm" htmlFor="Name">Name</Label>
+                <Input
+                  className="border-slate-500 focus:border-none"
+                  id="first-name"
+                  placeholder="Max"
+                  {...register("name", { required: "Name is required" })}
+                />
+                {errors.name && (
+                  <span className="text-red-500 text-xs">
+                    {errors.name.message}
+                  </span>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label className="text-sm" htmlFor="email">Email</Label>
+                <Input
+                  className="border-slate-500 focus:border-none"
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  {...register("email", { required: "Email is required" })}
+                />
+                {errors.email && (
+                  <span className="text-red-500 text-xs">
+                    {errors.email.message}
+                  </span>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label className="text-sm" htmlFor="password">Password</Label>
+                <Input
+                  className="border-slate-500 focus:border-none"
+                  id="password"
+                  type="password"
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                />
+                {errors.password && (
+                  <span className="text-red-500 text-xs">
+                    {errors.password.message}
+                  </span>
+                )}
+              </div>
+              <Button type="submit" className="bg-violet-800 text-white hover:bg-violet-900 hover:text-white" disabled={loading}>
+                {loading ? (
+                  <div className="flex items-center justify-center">
+                    <div className="w-4 h-4 border-2 border-t-2 border-white border-t-transparent rounded-full animate-spin mr-2"></div>
+                    Creating...
+                  </div>
+                ) : (
+                  "Create an account"
+                )}
+              </Button>
+            </form>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link to="/sign-in" className="underline">
+              Sign in
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default SignUp;
